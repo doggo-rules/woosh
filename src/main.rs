4c7eb711fd72a32be9259ebc74ad7d6369ecf53f
Doggo Rules <doggorules@protonmail.com>
2021-05-23 14:08:16 +0300

    Add history

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,19 @@ mod commands;
 mod config;
 mod handler;
 
-use std::{env::current_dir, process::exit};
+use std::{env::{self, current_dir}, process::exit};
 use rustyline::Editor;
 
 fn main() {
+    let home = env::var("HOME").unwrap();
     let mut editor = Editor::<()>::new();
+    let res = editor.load_history(&format!("{}/.woosh-history", home));
+
+    if let Err(_) = res {
+        println!("WARNING: Failed to load history file!");
+    }
+
+    println!("Welcome to \x1b[1;31mWoosh\x1b[0m!");
 
     let mut state = handler::Shell {
         cd: current_dir().unwrap().display().to_string()
@@ -17,9 +25,16 @@ fn main() {
         let input = editor.readline(conf.prompt.as_str());
 
         match input {
-            Ok(command) =>
-                handler::handle(command, &mut state),
-            Err(_) => exit(0)
+            Ok(command) => {
+                editor.add_history_entry(command.as_str());
+                handler::handle(command, &mut state);
+            },
+            Err(rustyline::error::ReadlineError::Interrupted) => {},
+            Err(_) => {
+                break;
+            }
         }
+
+        editor.save_history(&format!("{}/.woosh-history", home)).unwrap();
     }
 }
